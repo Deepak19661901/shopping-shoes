eb190b91fcd5f46b2cb6f4833b3f5cac6ff97880
deepak kumar <deepakkumarnieit@gmail.com>
2024-02-15 15:00:38 +0530

    add sorted button

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector} from 'react-redux';
 import ShoesCard from '../Components/ShoesCard';
-
+import SortedShoes from '../Components/SoretdShoes'
 
 const Home = () => {
   const shoesData = useSelector((state) => state.data.value);
@@ -11,7 +11,7 @@ const Home = () => {
   const renderData = filterdData.length>0 ? filterdData:shoesData
   return (
     <>
-
+    <SortedShoes/>
     <div className='w-full flex flex-wrap px-2 sm:px-4 md:px-6 lg:px-8 xl:px-10  pt-24 justify-center '>
    
       {
